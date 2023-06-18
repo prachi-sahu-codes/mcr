5acf2e68300682147cc8afdb375a03ec9fd12038
SahuPrachi <prachisahu998@gmail.com>
2023-06-18 18:04:43 +0530

    feat: routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { NavLink, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Home } from "./pages/Home";
+import { Archive } from "./pages/Archive";
+
+const getActiveStyle = ({ isActive }) => ({
+  color: isActive ? "#240046" : "#7b2cbf",
+  textDecoration: isActive ? "underline" : "",
+});
 
 function App() {
   return (
     <div className="App">
-      <h1>hello </h1>
+      <nav className="navigation">
+        <NavLink to="/" style={getActiveStyle} className="navLink">
+          Home
+        </NavLink>
+        <NavLink to="/archive" style={getActiveStyle} className="navLink">
+          Archive
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/archive" element={<Archive />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Your habits</h1>
+    </div>
+  );
+};
